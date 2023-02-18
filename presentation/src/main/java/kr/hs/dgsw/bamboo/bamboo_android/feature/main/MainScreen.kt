c88ae265c45404ea982e03848b2a1c76bc08281a
윤석규 <devSxxkgyu@gmail.com>
2023-02-18 19:53:44 +0900

    ✨ :: Add Navigation Route
    Jetpack Compose Navigation의 NavHost를 설정하였습니다.

---
+++ presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/feature/main/MainScreen.kt
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.bamboo.bamboo_android.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun MainScreen(
+    mainViewModel: MainViewModel = hiltViewModel()
+) {
+}
