074eae265afa6fba95038c85efaa7e40eea50f97
윤석규 <devSxxkgyu@gmail.com>
2023-02-21 02:08:15 +0900

    ✨ :: Add Post Models
    Post 와 PostList의 Model을 추가하였습니다.

---
+++ data/src/main/java/kr/hs/dgsw/bamboo/data/network/response/post/PostListResponse.kt
@@ -0,0 +1,9 @@
+package kr.hs.dgsw.bamboo.data.network.response.post
+
+import com.google.gson.annotations.SerializedName
+
+data class PostListResponse(
+    @SerializedName("list")
+    val list: List<PostResponse>,
+
+    )
---
+++ domain/src/main/java/kr/hs/dgsw/bamboo/domain/model/post/HashTag.kt
@@ -0,0 +1,6 @@
+package kr.hs.dgsw.bamboo.domain.model.post
+
+data class HashTag(
+    val hashTag: String,
+    val tagId: Long
+)
