ca3d8b97416aab6638deb414e716767e737e7d9b
윤석규 <devSxxkgyu@gmail.com>
2023-02-21 02:03:42 +0900

    ✨ :: Add SharedPreferences
    토큰 저장을 위해 sharedpreferences를 추가하였습니다.

---
+++ data/src/main/java/kr/hs/dgsw/bamboo/data/database/prefs/PreferenceManager.kt
@@ -0,0 +1,33 @@
+package kr.hs.dgsw.bamboo.data.database.prefs
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceManager(context: Context) {
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences(BAMBOO_APP, Context.MODE_PRIVATE)
+
+    var autoLogin: Boolean
+        get() = prefs.getBoolean(AUTO_LOGIN_KEY, false)
+        set(value) = prefs.edit().putBoolean(AUTO_LOGIN_KEY, value).apply()
+
+    var accessToken: String
+        get() = prefs.getString(ACCESS_TOKEN, "").toString()
+        set(value) = prefs.edit().putString(ACCESS_TOKEN, value).apply()
+
+    var refreshToken: String
+        get() = prefs.getString(REFRESH_TOKEN, "").toString()
+        set(value) = prefs.edit().putString(REFRESH_TOKEN, value).apply()
+
+    fun deleteToken() {
+        prefs.edit().remove(ACCESS_TOKEN).apply()
+        prefs.edit().remove(REFRESH_TOKEN).apply()
+    }
+
+    companion object {
+        const val BAMBOO_APP = "BAMBOO_APP"
+        const val AUTO_LOGIN_KEY = "AUTO_LOGIN_KEY"
+        const val ACCESS_TOKEN = "ACCESS_TOKEN"
+        const val REFRESH_TOKEN = "REFRESH_TOKEN"
+    }
+}
--- presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/util/BambooApplication.kt
+++ presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/util/BambooApplication.kt
@@ -2,6 +2,16 @@ package kr.hs.dgsw.bamboo.bamboo_android.util
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import kr.hs.dgsw.bamboo.data.database.prefs.PreferenceManager
 
 @HiltAndroidApp
-class BambooApplication : Application()
+class BambooApplication : Application() {
+    companion object {
+        lateinit var prefs: PreferenceManager
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        prefs = PreferenceManager(applicationContext)
+    }
+}
