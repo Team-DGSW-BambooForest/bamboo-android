d61b3b518be174d51ae5196df9dd3a6767bf06cb
윤석규 <greeentea.dev@gmail.com>
2023-04-04 13:58:35 +0900

    ✨ :: Add SearchPostUseCase Module

--- presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/di/UseCaseModule.kt
+++ presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/di/UseCaseModule.kt
@@ -9,6 +9,7 @@ import kr.hs.dgsw.bamboo.domain.repository.UploadRepository
 import kr.hs.dgsw.bamboo.domain.usecase.CreatePostUseCase
 import kr.hs.dgsw.bamboo.domain.usecase.GetImageUseCase
 import kr.hs.dgsw.bamboo.domain.usecase.GetPostListUseCase
+import kr.hs.dgsw.bamboo.domain.usecase.SearchPostUseCase
 import javax.inject.Singleton
 
 @Module
@@ -29,4 +30,9 @@ object UseCaseModule {
     @Provides
     fun provideCreatePostUseCase(repository: PostRepository): CreatePostUseCase =
         CreatePostUseCase(repository)
+
+    @Singleton
+    @Provides
+    fun provideSearchPostUseCase(repository: PostRepository): SearchPostUseCase =
+        SearchPostUseCase(repository)
 }
