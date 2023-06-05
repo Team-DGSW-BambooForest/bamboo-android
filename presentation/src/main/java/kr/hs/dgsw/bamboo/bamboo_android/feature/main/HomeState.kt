a07958abd9e7af845ccda7294fa741999f5af2ec
윤석규 <greeentea.dev@gmail.com>
2023-06-05 13:28:38 +0900

    🔥 :: Remove compose codes

--- presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/feature/main/HomeState.kt
+++
@@ -1,9 +0,0 @@
-package kr.hs.dgsw.bamboo.bamboo_android.feature.main
-
-import kr.hs.dgsw.bamboo.domain.entity.post.PostList
-
-data class HomeState(
-    val postList: PostList? = null,
-    val contentImage: String? = null,
-    val exception: Throwable? = null,
-)
--- presentation/src/main/java/kr/hs/dgsw/bamboo/bamboo_android/feature/search/SearchState.kt
+++
@@ -1,9 +0,0 @@
-package kr.hs.dgsw.bamboo.bamboo_android.feature.search
-
-import kr.hs.dgsw.bamboo.domain.entity.post.PostList
-
-data class SearchState(
-    val postList: PostList? = null,
-    val contentImage: String? = null,
-    val exception: Throwable? = null,
-)
